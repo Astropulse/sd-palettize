02a143b3cffade75ac0cd652db74d8353f078630
Astropulse <61034487+Astropulse@users.noreply.github.com>
2024-04-18 13:26:41 -0400

    Fix requirements install

--- install.py
+++ install.py
@@ -6,5 +6,10 @@ req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requiremen
 with open(req_file) as file:
     for lib in file:
         lib = lib.strip()
-        if not launch.is_installed(lib):
+        urlparts = lib.split("#egg=")
+        if len(urlparts) > 1:
+            packname = urlparts[1]
+        else:
+            packname = "hitherdither"
+        if not launch.is_installed(packname):
             launch.run_pip(f"install {lib}", f"sd-palettize requirement: {lib}")
